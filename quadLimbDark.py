bb5e391fe8876af555deeee8437c173514c0d7fa
Eduard Larranaga <eduard@Eduards-MacBook-Air.local>
2021-07-30 10:55:19 -0500

    Complete transit lightcurve generated

--- quadLimbDark.py
+++ quadLimbDark.py
@@ -10,7 +10,14 @@ def L(r, P, Z):
   Obstruction function
   '''
   p = P/r
-  z = Z/r
+  z0 = Z/r
+  
+  # Reflects the information w.r.t. the y axis  
+  if z0>0:
+    z = z0
+  if z0<0:
+    z = -z0
+  
   if z>1+p:
     return 0.
   elif (abs(1-p)<z and z<=1+p):
@@ -21,7 +28,7 @@ def L(r, P, Z):
     return (L0 + k1 - L2)*r**2/np.pi
   elif (z<=1-p):
     return p**2*r**2
-  elif z<= p-1: # Check this option!!
+  elif z<= p-1: 
     return 1.*r**2
 
 
@@ -54,20 +61,24 @@ gamma2 = 0.34
 p=0.1
 
 # Discretization grid
-N = 100
-z_range = np.linspace(0.,1.2,N)
+N = 400
+z_max = 3.
+z_min = - z_max
+z_range = np.linspace(z_min,z_max,N)
 F = np.zeros(N)
 
 # Main Loop to calculate the Flux
 for i in range(N):
-  Integral_1 = integrate.quad(integrand_1, 0.0001, 1., args=(p,z_range[i]))[0]
-  Integral_2 = integrate.quad(integrand_2, 0.0001, 1.)[0]
+  Integral_1 = integrate.quad(integrand_1, 0.0, 1., args=(p,z_range[i]))[0]
+  Integral_2 = integrate.quad(integrand_2, 0.0, 1.)[0]
   F[i] = 1. - Integral_1/Integral_2
 
 # Plot the Flux as function of z
 plt.figure(figsize=(7,7))
 plt.plot(z_range, F)
-plt.hlines(1., 0., 1.2,linestyle='dashed', alpha=0.3)
+plt.hlines(1., z_min, z_max,linestyle='dashed', alpha=0.3)
 plt.xlabel(r'$z$')
 plt.ylabel(r'$F(p,z)$')
+plt.title('Exoplanet Transit Lightcurve')
+plt.savefig('lightcurve.jpg')
 plt.show()

213c25a91b8209efd819a862cdb42c8fd2b9c9b2
Eduard Larranaga <eduard@Eduards-MacBook-Air.local>
2021-08-05 16:19:05 -0500

    Updated version of the lightcurve plot

--- quadLimbDark.py
+++ quadLimbDark.py
@@ -1,3 +1,4 @@
+# Exoplanet transit lightcurve with
 # Quadratic Limb Darkening Model
 
 from scipy.misc import derivative
@@ -12,12 +13,12 @@ def L(r, P, Z):
   p = P/r
   z0 = Z/r
   
-  # Reflects the information w.r.t. the y axis  
+  # Reflects the curve w.r.t. the y axis  
   if z0>0:
     z = z0
   if z0<0:
     z = -z0
-  
+    
   if z>1+p:
     return 0.
   elif (abs(1-p)<z and z<=1+p):
@@ -55,30 +56,51 @@ def integrand_2(r):
   '''
   return I_function(r)*2*r
 
-# Parameters for the model
-gamma1 = 0.296 
-gamma2 = 0.34
-p=0.1
 
-# Discretization grid
-N = 400
-z_max = 3.
-z_min = - z_max
-z_range = np.linspace(z_min,z_max,N)
-F = np.zeros(N)
+##############################################################
+# Parameters for the model
+p = 0.1 #radius ratio
+b = 0.7 #impact. parameter
+gamma1 = 0.296 #linear limb darkening 
+gamma2 = 0.34 #quadratic limb darkening
+a = 2. #semi-major axis
+T = 10. #orbital period
+delta = 0. #orbital phase
+omega = 2*np.pi/T #angular velocity
+
+# Grid definition
+N = 1000
+t = np.linspace(0.,30,N)
+x = a*np.cos(omega*t+delta)
+
+z_range = np.sqrt(x**2 + b**2)
+
+
+# Flux
+F1 = np.zeros(N)
+F2 = np.zeros(N)
+
 
 # Main Loop to calculate the Flux
 for i in range(N):
-  Integral_1 = integrate.quad(integrand_1, 0.0, 1., args=(p,z_range[i]))[0]
-  Integral_2 = integrate.quad(integrand_2, 0.0, 1.)[0]
-  F[i] = 1. - Integral_1/Integral_2
+  Integral_1 = integrate.quad(integrand_1, 0.0001, 1., args=(p,x[i]))[0]
+  Integral_2 = integrate.quad(integrand_2, 0.0001, 1.)[0]
+  F1[i] = 1. - Integral_1/Integral_2
 
-# Plot the Flux as function of z
-plt.figure(figsize=(7,7))
-plt.plot(z_range, F)
-plt.hlines(1., z_min, z_max,linestyle='dashed', alpha=0.3)
-plt.xlabel(r'$z$')
+  Integral_1 = integrate.quad(integrand_1, 0.0001, 1., args=(p,z_range[i]))[0]
+  Integral_2 = integrate.quad(integrand_2, 0.0001, 1.)[0]
+  F2[i] = 1. - Integral_1/Integral_2
+
+
+
+# Plot the Flux as function of time
+plt.figure(figsize=(10,7))
+plt.plot(t, F1,color='cornflowerblue', label=f'b = 0.0')
+plt.plot(t, F2,color='crimson', label=f'b = {b:.1f}')
+plt.hlines(1., t[0], t[-1],linestyle='dashed', alpha=0.3)
+plt.xlabel(r'$t$')
 plt.ylabel(r'$F(p,z)$')
 plt.title('Exoplanet Transit Lightcurve')
+plt.legend()
 plt.savefig('lightcurve.jpg')
 plt.show()
